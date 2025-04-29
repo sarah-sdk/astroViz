cf62a17bc7ff43831eba87ae865663b275fbcf30
Sarah Smandack <sarah.smandack@gmail.com>
2025-04-29 21:24:41 +0200

    interplanetary age backend done

--- backend/calculs/urls.py
+++ backend/calculs/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
   path('planets/', views.planet_list, name='planet_list'),
+  path('interplanetary-age/', views.interplanetary_age, name="interplanetary_age"),
 ]
--- backend/calculs/views.py
+++ backend/calculs/views.py
@@ -18,3 +18,27 @@ def planet_list(request):
   ]
 
   return JsonResponse(data, safe=False)
+
+def interplanetary_age(request):
+  try:
+    age_earth = float(request.GET.get("age", ""))
+  except (TypeError, ValueError):
+    return JsonResponse({"error": "Invalid or missing 'age' parameter."}, status=400)
+  
+  planets = Planet.objects.all()
+  result = []
+
+  for planet in planets:
+    if planet.name.lower() == "terre":
+      result.append({
+        "planet": planet.name,
+        "age_on_planet": age_earth,
+      })
+    elif planet.orbital_period > 0:
+      age_on_planet = age_earth * ( 365.25 / planet.orbital_period)
+      result.append({
+        "planet": planet.name,
+        "age_on_planet": round(age_on_planet, 2)
+      })
+
+  return JsonResponse({"interplanetary_age": result})
