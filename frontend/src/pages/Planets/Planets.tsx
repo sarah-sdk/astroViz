8dae7ad5fc7d480504a3a780f2ab218917c9f6aa
Sarah Smandack <sarah.smandack@gmail.com>
2025-04-29 19:29:02 +0200

    create .env and try to fetch planets list

--- frontend/src/pages/Planets/Planets.tsx
+++ frontend/src/pages/Planets/Planets.tsx
@@ -1,3 +1,16 @@
+import { useEffect, useState } from "react";
+
 export default function Planets() {
+  const [planets, setPlanets] = useState([]);
+
+  useEffect(() => {
+    const fetchPlanet = async () => {
+      const data = await fetch(`${import.meta.env.VITE_API_URL}/planets`);
+      console.log(data);
+    };
+
+    fetchPlanet();
+  }, []);
+
   return <h1>Pouet</h1>;
 }
