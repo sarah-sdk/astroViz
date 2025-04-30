c062f0b38f6509bcd899a9dbfbceb13dc11beae7
Sarah Smandack <sarah.smandack@gmail.com>
2025-04-30 11:53:42 +0200

    time to sun calcul

--- backend/calculs/urls.py
+++ backend/calculs/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
   path('planets/', views.planet_list, name='planet_list'),
   path('interplanetary-age/', views.interplanetary_age, name="interplanetary_age"),
+  path('time-to-sun/', views.time_to_sun, name='time_to_sun')
 ]
--- backend/calculs/views.py
+++ backend/calculs/views.py
@@ -42,3 +42,34 @@ def interplanetary_age(request):
       })
 
   return JsonResponse({"interplanetary_age": result})
+
+def time_to_sun(request):
+  light_speed = 299_792  # en km/s
+
+  planet_request = request.GET.get("planet", "").lower()
+  if not planet_request:
+    return JsonResponse({"error": "Missing 'planet' parameter."}, status=400)
+  
+  try:
+    planet = Planet.objects.get(name__iexact=planet_request)
+  except Planet.DoesNotExist:
+    return JsonResponse({"error": f"Planet '{planet_request}' not found."}, status=404)
+  
+  time = round(planet.distance_from_sun * 10**6/ light_speed, 2)
+
+  hours_time = round(time // 3600)
+  minutes_time = round((time % 3600) // 60)
+  secondes_time = round(time % 60)
+
+  part = []
+
+  if hours_time:
+    part.append(f'{hours_time} heure' + ("s" if hours_time > 1 else ""))
+  if minutes_time:
+    part.append(f'{minutes_time} minute' + ("s" if minutes_time > 1 else ""))
+  if secondes_time:
+    part.append(f'{secondes_time} seconde' + ("s" if secondes_time > 1 else ""))
+
+  return JsonResponse({"time_to_sun": " ".join(part)})
+
+
